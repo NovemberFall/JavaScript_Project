34d3511358d10c7c303e8cd68599881da33f6451
Git <1219.zheng@gmail.com>
2020-01-10 15:03:47 -0800

    update

--- test/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './scripts/ui.js',
-    output: {
-        path: path.resolve(__dirname, 'dist/assets/'),
-        filename: 'uiAfter.js'
-    }
-};
